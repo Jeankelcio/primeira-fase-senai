2f7bf4fbc1099d475860dc2158af159ff286ad39
JEAN KELCIO CARVALHO <jean_k_carvalho@estudante.sesisenai.org.br>
2025-09-09 22:15:19 -0300

    variaveis em javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,23 @@
+//variavel com var
+//var = jean
+// var nome = "jean"
+//mome = jean
+
+//variavel com let
+// let nome = "jean"
+//  nome = "jean"
+
+//variavel declarada com const
+const nome ="jean" //string
+const sobrenome ="kelcio"
+const idade = 100 //number
+const masculino = true //boolean
+
+// console.log("Meu nome e " + nome + " " + sobrenome + " e minha idade e " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+
+//array
+const frutas =["laranja","maçã","banana"]
+
+console.log(frutas[0])
